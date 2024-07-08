893231ab4a800f26faf30a74bf4e804c45aaf29b
Jefferson Santos Angulo <jsantos@yawi.pe>
2024-07-07 21:42:16 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './auth/pages/loginPage/loginPage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path : '', redirectTo:'login', pathMatch: 'full'},
+  {path : '**', redirectTo:'login'},
+  {path :'login', component: LoginPageComponent}
+];

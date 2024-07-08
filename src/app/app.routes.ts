68a33d82068e8c49a59feb15cd7f4fe192f21553
Lucas Barrenechea <lucascode07@gmail.com>
2024-07-07 23:57:33 -0500

    feat: se agrega página de registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './auth/pages/loginPage/loginPage.component';
 
 export const routes: Routes = [
-  {path : '', redirectTo:'login', pathMatch: 'full'},
-  {path : '**', redirectTo:'login'},
-  {path :'login', component: LoginPageComponent}
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/pages/register-page/register-page.component').then(
+        (c) => c.RegisterPageComponent
+      ),
+  },
+  { path: '**', redirectTo: 'login' },
 ];
---
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss',
+})
+export class RegisterPageComponent {}
